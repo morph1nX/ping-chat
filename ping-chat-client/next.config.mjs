b01574610333577bc35fb1626421961b1c7a8678
Rhythm Aich <rhythmaich@gmail.com>
2024-10-02 02:39:04 +0530

    changes to snv variables

--- ping-chat-client/next.config.mjs
+++ ping-chat-client/next.config.mjs
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const nextConfig = {
   output: "export",
-  env: {
-    apiKey: "AIzaSyDsRdbKrjki2HZKcs7YiJF3dFTLYSXXdMc",
+  //   env: {
+  //     apiKey: process.env.FIREBASE_API_KEY,
 
-    authDomain: "ping-chat-web.firebaseapp.com",
+  //     authDomain: "ping-chat-web.firebaseapp.com",
 
-    projectId: "ping-chat-web",
+  //     projectId: "ping-chat-web",
 
-    storageBucket: "ping-chat-web.appspot.com",
+  //     storageBucket: "ping-chat-web.appspot.com",
 
-    messagingSenderId: "694422433583",
+  //     messagingSenderId: "694422433583",
 
-    appId: "1:694422433583:web:18fbfd1a46d10b2339bbda",
-  },
+  //     appId: "1:694422433583:web:18fbfd1a46d10b2339bbda",
+  //   },
 };
 
 export default nextConfig;
