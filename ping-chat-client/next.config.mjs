7d67353e0378081e2801643d11cce89a01aa6566
Rhythm Aich <rhythmaich@gmail.com>
2024-10-02 02:42:20 +0530

    changes

--- ping-chat-client/next.config.mjs
+++ ping-chat-client/next.config.mjs
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const nextConfig = {
   output: "export",
-  //   env: {
-  //     apiKey: process.env.FIREBASE_API_KEY,
-
-  //     authDomain: "ping-chat-web.firebaseapp.com",
-
-  //     projectId: "ping-chat-web",
-
-  //     storageBucket: "ping-chat-web.appspot.com",
-
-  //     messagingSenderId: "694422433583",
-
-  //     appId: "1:694422433583:web:18fbfd1a46d10b2339bbda",
-  //   },
 };
 
 export default nextConfig;
